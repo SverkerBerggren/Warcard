9b2ec22d988d21ede632772d17ccaef261ea5a62
MrBoboGet <emanuelberggren01@gmail.com>
2022-05-04 23:57:49 +0200

    Initial commit

---
+++ Source/WarCard/WarCardGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "WarCardGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class WARCARD_API AWarCardGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
