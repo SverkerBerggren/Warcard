13be97a2ab9959c92e73299e37b83ecea2540d7f
SverkerBerggren <svartaspjutet@gmail.com>
2022-05-08 04:52:26 +0200

    UiGrejer

---
+++ Source/WarCard/Private/MyClassaosdjaisdjidjsoijido.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyClassaosdjaisdjidjsoijido.h"
+
+MyClassaosdjaisdjidjsoijido::MyClassaosdjaisdjidjsoijido()
+{
+}
+
+MyClassaosdjaisdjidjsoijido::~MyClassaosdjaisdjidjsoijido()
+{
+}
